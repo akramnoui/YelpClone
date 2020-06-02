000edc1e9bbfa54f04b4aa059e1cf978c3144670
Akram Noui <im_noui@esi.dz>
2020-06-02 23:44:39 +0100

    modularizing & passing infos between components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,15 +4,37 @@ import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
+
+
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington ',
+  city: 'Flavortown 101',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italians',
+  rating: 4.5,
+  reviewCount: 90
+
+}
+
+
+const businesses = [
+  business,
+  business , business , business, business , business
+];
+
 function App() {
   return (
 
       <div className="App">
           <h1>ravenous</h1>
             <SearchBar/>
-            <BusinessList/>
+            <BusinessList businesses={businesses}  />
       </div>
   );
 }
+
 
 export default App;
